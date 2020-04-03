896ba4c85c56d46eb6f62f978d670a5ff7b61178
shioshi <soltyna28@gmail.com>
2020-04-03 09:12:22 +0900

    [Update]12章]

---
+++ Ajax-Lesson/script.js
@@ -0,0 +1,31 @@
+$(function(){
+  $('#btn').on('click',function(){
+
+  	$.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "c39f8942a1f2b1e329972279ce6f75dd",
+      dataType : 'jsonp',
+    
+    }).done(function (data){
+
+      $('#place').text(data.name);
+
+      $('#temp_max').text(data.main.temp_max);
+
+      $('#temp_min').text(data.main.temp_min);
+
+      $('#humidity').text(data.main.humidity);
+
+      $('#speed').text(data.wind.speed);
+
+      $('#weather').text(data.weather[0].main);
+
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+
+    }).fail(function(data){
+
+    alert('通信に失敗しました。');
+
+    })
+  });
+});
